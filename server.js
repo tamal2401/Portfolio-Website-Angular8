3b2b9028f0770ad079784e8003003370fedb5e30
Tamal2401 <onlyjohn98@gmail.com>
2020-07-05 15:19:06 +0530

    commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 var path = require("path");
 const app= express();
 
-app.use(express.static(__dirname+'/dist'));
+app.use(express.static(__dirname+'/dist/portfolio-website'));
 
 app.listen(process.env.PORT||8080)
 
